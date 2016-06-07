bd141ca30411344b3b39f76e6cb351ccc5fe8f20
Martin Mroz <martinmroz@gmail.com>
2016-06-06 23:56:20 -0700

    Tests for all basic Reader error cases.

--- tests/test_reader.rs
+++ tests/test_reader.rs
@@ -6,30 +6,6 @@ use ihex::record::Record;
 use ihex::reader::*;
 
 #[test]
-fn test_record_from_record_string_parses_valid_data_records() {
-  assert_eq!(Record::from_record_string(":0B0010006164647265737320676170A7"),
-    Ok(Record::Data {
-      offset: 0x0010,
-       value: vec![0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x20, 0x67, 0x61, 0x70]
-    })
-  );
-
-  assert_eq!(Record::from_record_string(":00FFFE0003"),
-    Ok(Record::Data {
-      offset: 0xFFFE,
-       value: vec![]
-    })
-  );
-}
-
-// /// The record is not the length it claims.
-// PayloadLengthMismatch,
-// /// The record type is not supported.
-// UnsupportedRecordType(u8),
-// /// The payload length does not match the record type.
-// InvalidLengthForType,
-
-#[test]
 fn test_record_from_record_string_rejects_missing_start_code() {
   assert_eq!(Record::from_record_string("00000001FF"), Err(ReaderError::MissingStartCode));
 }
@@ -74,13 +50,60 @@ fn test_record_from_record_string_rejects_non_hex_characters() {
 }
 
 #[test]
-fn test_record_from_record_string_detects_invalid_checksums() {
+fn test_record_from_record_string_rejects_invalid_checksums() {
   assert_eq!(Record::from_record_string(":0B0010006164647265737320676170FF"), Err(ReaderError::ChecksumMismatch(0xA7,0xFF)));
   assert_eq!(Record::from_record_string(":0000000100"), Err(ReaderError::ChecksumMismatch(0xFF,0x00)));
   assert_eq!(Record::from_record_string(":020000021200EB"), Err(ReaderError::ChecksumMismatch(0xEA,0xEB)));
   assert_eq!(Record::from_record_string(":04000003000038001C"), Err(ReaderError::ChecksumMismatch(0xC1,0x1C)));
   assert_eq!(Record::from_record_string(":02000004FFFFFD"), Err(ReaderError::ChecksumMismatch(0xFC,0xFD)));
   assert_eq!(Record::from_record_string(":04000005000001CD2A"), Err(ReaderError::ChecksumMismatch(0x29,0x2A)));
+}
+
+#[test]
+fn test_record_from_record_string_rejects_payload_length_mismatches() {
+  assert_eq!(Record::from_record_string(":0C0010006164647265737320676170A6"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":000010006164647265737320676170B2"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":01000001FE"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":0F0000021200DD"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":0200000300003800C3"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":01000004FFFFFD"), Err(ReaderError::PayloadLengthMismatch));
+  assert_eq!(Record::from_record_string(":05000005000001CD28"), Err(ReaderError::PayloadLengthMismatch));
+}
+
+#[test]
+fn test_record_from_record_string_rejects_unsupported_record_types() {
+  assert_eq!(Record::from_record_string(":0B0010066164647265737320676170A1"), Err(ReaderError::UnsupportedRecordType(0x06)));
+  assert_eq!(Record::from_record_string(":0B0010FF6164647265737320676170A8"), Err(ReaderError::UnsupportedRecordType(0xFF)));
+}
+
+#[test]
+fn test_record_from_record_string_rejects_invalid_lengths_for_types() {
+  assert_eq!(Record::from_record_string(":01000001FFFF"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":0100000200FD"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":03000002FF1200EA"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":03000003003800C2"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":050000030000003800C0"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":01000004FFFC"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":03000004FFFFFFFC"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":030000050000CD2B"), Err(ReaderError::InvalidLengthForType));
+  assert_eq!(Record::from_record_string(":0500000500000000CD29"), Err(ReaderError::InvalidLengthForType));
+}
+
+#[test]
+fn test_record_from_record_string_parses_valid_data_records() {
+  assert_eq!(Record::from_record_string(":0B0010006164647265737320676170A7"),
+    Ok(Record::Data {
+      offset: 0x0010,
+       value: vec![0x61, 0x64, 0x64, 0x72, 0x65, 0x73, 0x73, 0x20, 0x67, 0x61, 0x70]
+    })
+  );
+
+  assert_eq!(Record::from_record_string(":00FFFE0003"),
+    Ok(Record::Data {
+      offset: 0xFFFE,
+       value: vec![]
+    })
+  );
 }
 
 #[test]
@@ -92,10 +115,12 @@ fn test_record_from_record_string_parses_valid_eof_record() {
 #[test]
 fn test_record_from_record_string_parses_valid_extended_segment_address() {
   assert_eq!(Record::from_record_string(":0200000212FEEC"), Ok(Record::ExtendedSegmentAddress(0x12FE)));
+  assert_eq!(Record::from_record_string(":0200000212fEEc"), Ok(Record::ExtendedSegmentAddress(0x12FE)));
 }
 
 #[test]
 fn test_record_from_record_string_parses_valid_start_segment_address() {
+  assert_eq!(Record::from_record_string(":04000003123438007B"), Record::from_record_string(":04000003123438007b"));
   assert_eq!(Record::from_record_string(":04000003123438007B"),
     Ok(Record::StartSegmentAddress {
       cs: 0x1234,
@@ -107,11 +132,13 @@ fn test_record_from_record_string_parses_valid_start_segment_address() {
 #[test]
 fn test_record_from_record_string_parses_valid_extended_linear_address() {
   assert_eq!(Record::from_record_string(":02000004ABCD82"), Ok(Record::ExtendedLinearAddress(0xABCD)));
+  assert_eq!(Record::from_record_string(":02000004abcd82"), Ok(Record::ExtendedLinearAddress(0xABCD)));
 }
 
 #[test]
 fn test_record_from_record_string_parses_valid_start_linear_address() {
   assert_eq!(Record::from_record_string(":0400000512345678E3"), Ok(Record::StartLinearAddress(0x12345678)));
+  assert_eq!(Record::from_record_string(":0400000512345678e3"), Ok(Record::StartLinearAddress(0x12345678)));
 }
 
 #[test]
